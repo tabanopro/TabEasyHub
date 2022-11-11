5a52542d64073faa930e3c943a2ecb72ef56ecb8
tabanopro <82659377+tabanopro@users.noreply.github.com>
2022-11-11 17:30:52 +0700

    Create instantuploadscript.lua

---
+++ instantuploadscript.lua
@@ -0,0 +1,10 @@
+--First is fast upload
+while true do
+	game:GetService("ReplicatedStorage").Events.UploadCurrentMemes:FireServer()
+	task.wait()
+
+	--seceond is auto upload
+--nah
+end
+
+--Credit to tabanopro1234 and SimpleSpy!
